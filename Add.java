38601df84bd77b6a9f1777795c7597591db39fb5
Jishnu Rajbanshi <jishnurajbanshi70@gmail.com>
2024-06-19 19:53:46 +0530

    "Add" Modified

--- Add.java
+++ Add.java
@@ -1,19 +1,14 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
-public class Add {
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
+public class Add 
+{
+    public static void main(String[] args) throws IOException 
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter the first number: ");
-        String input1 = reader.readLine();
-        int num1 = Integer.parseInt(input1);
-
+        int num1 = Integer.parseInt(br.readLine());
         System.out.print("Enter the second number: ");
-        String input2 = reader.readLine();
-        int num2 = Integer.parseInt(input2);
+        int num2 = Integer.parseInt(br.readLine());
         int sum = num1 + num2;
         System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
